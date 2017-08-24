c46431a8661f405ed5104b2c3753b4016bf3910b
manosf <pulsar@nebula.lan>
2017-08-24 23:47:18 +0300

    Added some arguments and exporting times option.

--- sctimer.py
+++ sctimer.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 import time
+import datetime
 import curses 
+import os
+import argparse
 
 #Initializing the curses module.
 stdscr=curses.initscr()
 curses.noecho()
 curses.cbreak()
 stdscr.nodelay(1)
+
+sct_parser=argparse.ArgumentParser(description='''
+            **A terminal based timer for SpeedCubing***
+            ''')
+sct_parser.add_argument('--countdown', action='store_true',
+                help='Disable the countdown function.')
+sct_parser.add_argument('-f', dest='filename',
+                default=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'times.txt'),
+                help='Specify file where your solve times will be exported.')
+
+sct_options = sct_parser.parse_args()
 
 solves = []
 
@@ -32,21 +46,29 @@ def stopwatch(t):
             time.sleep(0.01)
             t+=0.01
     except KeyboardInterrupt:
+        print
         return t
+
+def export_times(filepath, current_solves):
+    with open(filepath, 'w' if not os.path.isfile(filepath) else 'a') as times_file:
+        times_file.write(str(datetime.datetime.now().date())+': '+str(current_solves)+'\n')
     
 def main():
     while 1:
         key=stdscr.getch()
         if key==ord(' '):   #The solve count starts after pressing SPACEBAR
-            countdown()
+            if not sct_options.countdown:
+                countdown()
             solves.append(time_format(stopwatch(0.00)))
             print ("\rYour solves for this session: {0}".format(solves), end="\n\r")
         elif key==27:       #The application exits after pressing ESCAPE
+            if len(solves)>0:
+                export_times(sct_options.filename, solves)
             break
             
 if __name__=="__main__":
     main()
-    
+
 #Terminating properly the application by reversing the curses settings.    
 curses.echo()
 curses.nocbreak()
