78beebace4632d89e2eb71a8e9e7afe49e0e8434
Manos Fokas <31221999+manosf@users.noreply.github.com>
2017-08-25 20:32:58 +0300

    Fixed keypress control and termination issues.

--- sctimer.py
+++ sctimer.py
@@ -25,13 +25,23 @@ sct_options = sct_parser.parse_args()
 solves = []
 
 def countdown():
-    for num in range(15, 0, -1):
-        if num<=9:
-            print ('0'+'{0}'.format(num), end="\r")
-        else:
-            print ('{0}'.format(num), end="\r")
-        if num!=0:
-            time.sleep(1)
+    try:
+        key=None
+        for num in range(15, 0, -1):
+            if key!=ord(' '):
+                key=stdscr.getch()
+                if num !=0:
+                    if num>9:
+                        print ('{0}'.format(num), end="\r")
+                    else:
+                        print ('0'+'{0}'.format(num), end="\r")
+                    time.sleep(1)
+                else:
+                    key=ord(' ')
+            else:
+                break
+    except KeyboardInterrupt:
+        termination_handler()
 
 def time_format(time):
     if time == None:
@@ -41,35 +51,42 @@ def time_format(time):
 
 def stopwatch(t):
     try:
-        while 1:
+        key=None
+        while key!=ord(' '):
+            key=stdscr.getch()
             print(time_format(t), end="\r")
             time.sleep(0.01)
             t+=0.01
-    except KeyboardInterrupt:
-        print
         return t
+    except KeyboardInterrupt:
+        termination_handler()
 
 def export_times(filepath, current_solves):
     with open(filepath, 'w' if not os.path.isfile(filepath) else 'a') as times_file:
         times_file.write(str(datetime.datetime.now().date())+': '+str(current_solves)+'\n')
-    
+
+#Terminating properly the application by reversing the 'curses' settings
+def termination_handler():
+    curses.echo()
+    curses.nocbreak()
+    curses.endwin()
+    raise SystemExit
+
 def main():
     while 1:
-        key=stdscr.getch()
-        if key==ord(' '):   #The solve count starts after pressing SPACEBAR
-            if not sct_options.countdown:
-                countdown()
-            solves.append(time_format(stopwatch(0.00)))
-            print ("\rYour solves for this session: {0}".format(solves), end="\n\r")
-        elif key==27:       #The application exits after pressing ESCAPE
-            if len(solves)>0:
-                export_times(sct_options.filename, solves)
-            break
-            
+        try:
+            key=stdscr.getch()
+            if key==ord(' '):   #The solve count starts after pressing SPACEBAR
+                if not sct_options.countdown:
+                    countdown()
+                solves.append(time_format(stopwatch(0.00)))
+                print ("\rYour solves for this session: {0}".format(solves), end="\n\r")
+            elif key==27:       #The application exits after pressing ESCAPE
+                if len(solves)>0:
+                    export_times(sct_options.filename, solves)
+                termination_handler()
+        except KeyboardInterrupt:
+            termination_handler()
+    
 if __name__=="__main__":
     main()
-
-#Terminating properly the application by reversing the curses settings.    
-curses.echo()
-curses.nocbreak()
-curses.endwin()
